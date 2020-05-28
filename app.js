ca2356b238834b59e1f36c644781b86b54eba8a3
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-28 08:51:31 +0200

    Setting the prototype of an object

--- app.js
+++ app.js
@@ -3,3 +3,28 @@ const numbers = [1, 2, 3]; // new Array()
 // automatically when creating a new array
 const doubledNumbers = numbers.map(number => number * 2);
 console.log(doubledNumbers);
+
+
+const companyMember = {
+  company: 'Google',
+  greet() {
+    console.log('Hi!');
+  }
+}
+
+const employee = {
+  name: 'Gaetan',
+  age: 31,
+}
+
+const boss = {
+  name: 'Seb',
+  age: 42,
+  title: 'CEO'
+}
+
+employee.__proto__ = companyMember;
+boss.__proto__ = companyMember;
+
+employee.greet();
+console.log(boss.company);
