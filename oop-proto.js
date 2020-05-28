fbdaee0719f3f8b5d1c059f3f765e49e3511b8c4
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-28 10:04:44 +0200

    Patching the object prototype

--- oop-proto.js
+++ oop-proto.js
@@ -9,7 +9,14 @@ function Employee(name, age) {
   this.age = age;
 }
 
+// This sets the prototype of the to be created object
 Employee.prototype = companyPerson;
 
 const emp1 = new Employee('Gaetan', 31);
 emp1.greet();
+
+Employee.prototype.sayGoodBye = () => {
+  console.log('Bye');
+}
+
+emp1.sayGoodBye();
